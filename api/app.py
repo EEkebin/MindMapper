b183447ee7804fcf1b6cc6e90bf091205099224a
kmanouki <kebinImports@gmail.com>
2023-04-08 02:29:17 -0700

    Fixed API Integration

--- api/app.py
+++ api/app.py
@@ -13,6 +13,7 @@ conn = psycopg2.connect(
     port=5432
 )
 
+
 def encrypt_password(password: str) -> str:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
@@ -23,108 +24,158 @@ def verify_password(password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 
-@app.route('/')
+@app.route('/api')
 def hello_world():
-    return 'Welcome to MindMapper!'
+    return 'Welcome to MindMapper API!'
 
 
 @app.route('/api/create_user/<username>/<password>', methods=['POST'])
 def create_user(username, password):
     cur = conn.cursor()
-    passwordhash = encrypt_password(password)
-    cur.execute("INSERT INTO user_credentials (username, password) VALUES (%s, %s)",
-                (username, passwordhash))
-    conn.commit()
-    return 'User created!'
+    try:
+        passwordhash = encrypt_password(password)
+        cur.execute("INSERT INTO user_credentials (username, password) VALUES (%s, %s)",
+                    (username, passwordhash))
+        conn.commit()
+        return 'User created!'
+    except Exception as e:
+        # Handle the exception here
+        conn.rollback()
+        return 'Error: ' + str(e)
+    finally:
+        cur.close()
 
 
 # Delete user
 @app.route('/api/delete_user/<username>/<password>', methods=['DELETE'])
 def delete_user(username, password):
     cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM user_credentials WHERE username = %s", (username,))
-    user = cur.fetchone()
-    if verify_password(password, user[2]):
-        cur.execute(
-            "DELETE FROM user_credentials WHERE username = %s", (username,))
-        conn.commit()
-        return 'User deleted!'
-    else:
-        return 'Incorrect password!'
+    try:
+        cur.execute("SELECT * FROM user_credentials WHERE username = %s", (username,))
+        user = cur.fetchone()
+        if verify_password(password, user[2]):
+            cur.execute("DELETE FROM user_credentials WHERE username = %s", (username,))
+            conn.commit()
+            return 'User deleted!'
+        else:
+            return 'Incorrect password!'
+    except Exception as e:
+        # Handle the exception here
+        conn.rollback()
+        return 'Error: ' + str(e)
+    finally:
+        cur.close()
+
 
 
 # Get user
 @app.route('/api/get_user/<username>/<password>', methods=['GET'])
 def get_user(username, password):
     cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM user_credentials WHERE username = %s", (username,))
-    user = cur.fetchone()
-    
-    if verify_password(password, user[2]):
-        return str(user)
-    else:
-        return 'Incorrect password!'
+    try:
+        cur.execute("SELECT * FROM user_credentials WHERE username = %s", (username,))
+        user = cur.fetchone()
+        if user is None:
+            return 'User not found!'
+        if verify_password(password, user[2]):
+            return str(user)
+        else:
+            return 'Incorrect password!'
+    except Exception as e:
+        # Handle the exception here
+        return 'Error: ' + str(e)
+    finally:
+        cur.close()
+
 
 
 @app.route('/api/get_userid/<username>/<password>', methods=['GET'])
 def get_userid(username, password):
     cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM user_credentials WHERE username = %s", (username,))
-    user = cur.fetchone()
-    if verify_password(password, user[2]):
-        return str(user[0])
-    else:
-        return 'Incorrect password!'
+    try:
+        cur.execute("SELECT * FROM user_credentials WHERE username = %s", (username,))
+        user = cur.fetchone()
+        if user is None:
+            return 'User not found!'
+        if verify_password(password, user[2]):
+            return str(user[0])
+        else:
+            return 'Incorrect password!'
+    except Exception as e:
+        # Handle the exception here
+        return 'Error: ' + str(e)
+    finally:
+        cur.close()
+
 
 
 @app.route('/api/get_user_habits/<username>/<password>', methods=['GET'])
 def get_user_habits(username, password):
     cur = conn.cursor()
-    user_id = get_userid(username, password)
-    if user_id == 'Incorrect password!':
-        return 'Incorrect password!'
-    cur.execute("SELECT * FROM habits WHERE user_id = %s", (user_id,))
-    habits = cur.fetchall()
-    return str(habits)
+    try:
+        user_id = get_userid(username, password)
+        if user_id == 'Incorrect password!':
+            return 'Incorrect password!'
+        cur.execute("SELECT * FROM habits WHERE user_id = %s", (user_id,))
+        habits = cur.fetchall()
+        return str(habits)
+    except Exception as e:
+        # Handle the exception here
+        return 'Error: ' + str(e)
+    finally:
+        cur.close()
+
 
 
 @app.route('/api/create_habit/<username>/<password>/<habit_name>/<metric_name>/<metric_value>', methods=['POST'])
 def create_habit(username, password, habit_name, metric_name, metric_value):
     cur = conn.cursor()
-    user_id = get_userid(username, password)
-    if user_id == 'Incorrect password!':
-        return 'Incorrect password!'
-    cur.execute("INSERT INTO habits (user_id, habit_name, metric_name, metric_value) VALUES (%s, %s, %s, %s)",
-                (user_id, habit_name, metric_name, metric_value))
-    conn.commit()
-    return 'Habit created!'
+    try:
+        user_id = get_userid(username, password)
+        if user_id == 'Incorrect password!':
+            return 'Incorrect password!'
+        cur.execute("INSERT INTO habits (user_id, habit_name, metric_name, metric_value) VALUES (%s, %s, %s, %s)",
+                    (user_id, habit_name, metric_name, metric_value))
+        conn.commit()
+        return 'Habit created!'
+    except Exception as e:
+        # Handle the exception here
+        return 'Error: ' + str(e)
+    finally:
+        cur.close()
+
 
 
 @app.route('/api/delete_habit/<username>/<password>/<habit_name>', methods=['DELETE'])
 def delete_habit(username, password, habit_name):
-    cur = conn.cursor()
-    user_id = get_userid(username, password)
-    if user_id == 'Incorrect password!':
-        return 'Incorrect password!'
-    cur.execute(
-        "DELETE FROM habits WHERE user_id = %s AND habit_name = %s", (user_id, habit_name))
-    conn.commit()
-    return 'Habit deleted!'
+    try:
+        cur = conn.cursor()
+        user_id = get_userid(username, password)
+        if user_id == 'Incorrect password!':
+            return 'Incorrect password!'
+        cur.execute(
+            "DELETE FROM habits WHERE user_id = %s AND habit_name = %s", (user_id, habit_name))
+        conn.commit()
+        return 'Habit deleted!'
+    except Exception as e:
+        return str(e)
+
 
 
 @app.route('/api/update_habit/<username>/<password>/<habit_name>/<metric_name>/<metric_value>', methods=['PUT'])
 def update_habit(username, password, habit_name, metric_name, metric_value):
-    cur = conn.cursor()
-    user_id = get_userid(username, password)
-    if user_id == 'Incorrect password!':
-        return 'Incorrect password!'
-    cur.execute("UPDATE habits SET metric_name = %s, metric_value = %s WHERE user_id = %s AND habit_name = %s",
-                (metric_name, metric_value, user_id, habit_name))
-    conn.commit()
-    return 'Habit updated!'
+    try:
+        cur = conn.cursor()
+        user_id = get_userid(username, password)
+        if user_id == 'Incorrect password!':
+            return 'Incorrect password!'
+        cur.execute("UPDATE habits SET metric_name = %s, metric_value = %s WHERE user_id = %s AND habit_name = %s",
+                    (metric_name, metric_value, user_id, habit_name))
+        conn.commit()
+        return 'Habit updated!'
+    except Exception as e:
+        return str(e)
+
 
 
 # Get habit
@@ -134,28 +185,35 @@ def get_habit(username, password, habit_name):
     user_id = get_userid(username, password)
     if user_id == 'Incorrect password!':
         return 'Incorrect password!'
-    cur.execute(
-        "SELECT * FROM habits WHERE user_id = %s AND habit_name = %s", (user_id, habit_name))
+    cur.execute("SELECT * FROM habits WHERE user_id = %s AND habit_name = %s", (user_id, habit_name))
     habit = cur.fetchone()
-    return str(habit)
+    if habit:
+        return str(habit)
+    else:
+        return 'Habit not found!'
+
 
 
 # Get habit history
 @app.route('/api/get_habit_history/<username>/<password>/<habit_name>', methods=['GET'])
 def get_habit_history(username, password, habit_name):
-    cur = conn.cursor()
-    user_id = get_userid(username, password)
-    if user_id == 'Incorrect password!':
-        return 'Incorrect password!'
-    cur.execute(
-        "SELECT * FROM habits WHERE user_id = %s AND habit_name = %s", (user_id, habit_name))
-    habit = cur.fetchone()
-    habit_id = habit[0]
-    cur.execute(
-        "SELECT * FROM habits_history WHERE habit_id = %s", (habit_id,))
-    habit_history = cur.fetchall()
-    return str(habit_history)
+    try:
+        cur = conn.cursor()
+        user_id = get_userid(username, password)
+        if user_id == 'Incorrect password!':
+            return 'Incorrect password!'
+        cur.execute(
+            "SELECT * FROM habits WHERE user_id = %s AND habit_name = %s", (user_id, habit_name))
+        habit = cur.fetchone()
+        habit_id = habit[0]
+        cur.execute(
+            "SELECT * FROM habits_history WHERE habit_id = %s", (habit_id,))
+        habit_history = cur.fetchall()
+        return str(habit_history)
+    except Exception as e:
+        return "Error: " + str(e)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  let username = '';
+  let password = '';
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await fetch(`http://localhost:5000/api/create_user/${username}/${password}`, {
+      method: 'POST',
+    });
+
+    const data = await response.text();
+    console.log(data);
+    // Redirect to login page
+    window.location.href = '/login';
+  }
+</script>
+
+<form on:submit={handleSubmit}>
+  <input type="text" bind:value={username} placeholder="Username">
+  <input type="password" bind:value={password} placeholder="Password">
+  <button type="submit">Register</button>
+</form>
